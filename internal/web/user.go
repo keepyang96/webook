7c8928e238a0b0dfa3b8039de0e0f287e195a8b3
liuyang <422246421@qq.com>
2024-03-29 17:13:55 +0800

    注册用户路由

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,5 +1,32 @@
 package web
 
+import "github.com/gin-gonic/gin"
+
 // UserHandler 定义所有与User相关的路由
 type UserHandler struct {
+}
+
+// RegisterUserRoutes 注册用户相关路由
+func (u *UserHandler) RegisterUserRoutes(server *gin.Engine) {
+	// 路由分组简化路由
+	ug := server.Group("/users")
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
+}
+
+func (u *UserHandler) SignUp(c *gin.Context) {
+}
+
+func (u *UserHandler) Login(c *gin.Context) {
+
+}
+
+func (u *UserHandler) Edit(c *gin.Context) {
+
+}
+
+func (u *UserHandler) Profile(c *gin.Context) {
+
 }
